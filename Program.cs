b6241400952a5c302404d9ffade6dc5940d820ec
Nithyasubraman <144653596+Nithyasubraman@users.noreply.github.com>
2024-01-21 22:31:51 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace PatientManagementSystem
       {
         Console.WriteLine(" Welcome to Patient Management System ");
         Console.WriteLine();
-        //Console.WriteLine(" ************************************ ");
+        Console.WriteLine(" ************************************ ");
         Console.WriteLine();
         Console.WriteLine("1. Insert ");
         Console.WriteLine("2. Print ");
